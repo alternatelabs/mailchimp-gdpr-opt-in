0ee3b5e2c1848fda9376a8cc786a2df9093ee48f
Pete Hawkins <pete@alternatelabs.co>
2018-05-09 14:32:47 +0100

    Use ruby 2.5.1

--- Gemfile
+++ Gemfile
@@ -4,8 +4,13 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
+ruby "2.5.1"
 
 # Added at 2018-05-09 14:20:11 +0100 by pete:
-gem "sinatra", "~> 2.0"
+gem "sinatra", "~> 2.0", require: 'sinatra/base'
+gem "mailchimp-api", require: "mailchimp"
 gem "iodine"
+
+group :development, :test do
+  gem "dotenv"
+end
--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
-require "sinatra/base"
+# Default ENV to dev if not present
+ENV["APP_ENV"] ||= "development"
+
+# Autoload common standard library features and Gemfile gems
+require "json"
+require "bundler"
+Bundler.require :default, ENV["APP_ENV"].to_sym
+
+# Load dev environment variables
+Dotenv.load if %w[development test].include? ENV["APP_ENV"]
 
 class App < Sinatra::Base
   get "/consent" do
+    list_id = ENV.fetch("MAILCHIMP_LIST_ID")
+    api_key = ENV.fetch("MAILCHIMP_API_KEY")
+    mc = Mailchimp::API.new(api_key)
     email = params["email"]
+  rescue Mailchimp::Error => e
   end
 end
